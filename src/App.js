c033850d9cce8a3455bbe79e1a0ebf331d57a51b
Siddharth <2004sid@gmail.com>
2022-05-20 10:30:57 +0530

    info component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Info from "./info.js";
 
 function App() {
   return (
     <div className="App">
-      <p>Bare bones app</p>
+      <Info />
+      <AddItem />
     </div>
+  );
+}
+
+function AddItem() {
+  const value = "default";
+
+  return (
+    <form>
+      <label for="text-form">Type something: </label>
+      <input type="text" value={value} id="text-form" />
+    </form>
   );
 }
 
